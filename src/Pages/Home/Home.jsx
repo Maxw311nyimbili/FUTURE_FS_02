20037e865cba8d68e47b5d13ca404f8dc9fb00b5
Maxw311 <nyimbilimaxwell9@gmail.com>
2025-07-10 15:03:03 +0000

    restoration

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,7 @@
-import Banner from "../../Components/Banner/Banner";
-import Brand from "../../Components/Brand/Brand";
 import ClientAndRecentProducts from "../../Components/last/last";
-import Client from "../../Components/Client/Client";
-import Cards from "../../Components/Cards/Cards";
 import App1 from "../../Components/demo/demo";
 import App from './../../Components/App/App';
-import Features from './../../Components/Features/Features';
-import BerkshireHathaway from './../../Components/BerkshireHathaway/BerkshireHathaway';
+
 
 const Home = ({
     user,
