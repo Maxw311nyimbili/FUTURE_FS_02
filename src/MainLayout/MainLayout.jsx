278e6bb1575a971234b82992f85f8e28e05eb0ea
Maxw311 <nyimbilimaxwell9@gmail.com>
2025-07-12 03:15:32 +0200

    minor change

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -55,29 +55,29 @@ const MainLayout = () => {
     const handleSearchChange = (e) => setSearchQuery(e.target.value);
 
     // Cart handlers
-    // const addToCart = (product) => {
-    //     if (!user) {
-    //         setLoginModalAction('cart');
-    //         setLoginModalOpen(true);
-    //         return;
-    //     }
+    const addToCart = (product) => {
+        if (!user) {
+            setLoginModalAction('cart');
+            setLoginModalOpen(true);
+            return;
+        }
 
-    //     const existingItem = cartItems.find(item => item.id === product.id);
-    //     if (existingItem) {
-    //         setCartItems(cartItems.map(item =>
-    //             item.id === product.id
-    //                 ? { ...item, quantity: item.quantity + 1 }
-    //                 : item
-    //         ));
-    //     } else {
-    //         setCartItems([...cartItems, {
-    //             ...product,
-    //             quantity: 1,
-    //             name: product.title,
-    //             color: 'Default',
-    //         }]);
-    //     }
-    // };
+        const existingItem = cartItems.find(item => item.id === product.id);
+        if (existingItem) {
+            setCartItems(cartItems.map(item =>
+                item.id === product.id
+                    ? { ...item, quantity: item.quantity + 1 }
+                    : item
+            ));
+        } else {
+            setCartItems([...cartItems, {
+                ...product,
+                quantity: 1,
+                name: product.title,
+                color: 'Default',
+            }]);
+        }
+    };
 
     const updateCartQuantity = (id, quantity) => {
         if (quantity <= 0) {
